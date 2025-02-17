42bd16622abcd36a7fc66a3efe01078ffe57d8ed
dooydzx <dyd903013149@163.com>
2025-02-17 14:57:07 +0800

    Update EEG2Code_offline.py

--- EEG2Code_offline.py
+++ EEG2Code_offline.py
@@ -1,11 +1,6 @@
-# This program accomplishes the training of the EEG2Code model and subsequently predicts sequences from the test data.
-# In the runtime configuration, input the file to be processed, such as 'hljvmc.mat'.
-
 from __future__ import division
 import keras
 import time
-import os
-import gc
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,48 +8,16 @@ import scipy
 import scipy.io as sio
 import tensorflow as tf
 from keras import optimizers, Input
-from keras import initializers
 from keras.backend import permute_dimensions
 from keras.constraints import max_norm
 from keras.models import load_model, Model
-from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Permute, Flatten, Dense, BatchNormalization, Activation, Dropout, \
     AveragePooling2D, DepthwiseConv2D, Add, concatenate, Reshape
 from keras.utils import np_utils
-
-import keras
-import time
-import os
 import gc
-import sys
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy
-import scipy.io as sio
-import tensorflow as tf
-from keras import optimizers, Input
-from keras import initializers
-from keras.backend import permute_dimensions
-from keras.constraints import max_norm
-from keras.models import load_model, Model
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Permute, Flatten, Dense, BatchNormalization, Activation, Dropout, \
-    AveragePooling2D, DepthwiseConv2D, Add, concatenate, Reshape
-from keras.utils import np_utils
-from tensorflow.python.keras.layers import SpatialDropout2D, Lambda, SeparableConv2D
 
 
 def construct_model(windowSize, numberChannels):
-    """
-    Constructs the EEG2Code CNN model.
-
-    Args:
-        windowSize (int): The size of the time window for EEG data.
-        numberChannels (int): The number of EEG channels.
-
-    Returns:
-        model (keras.Model): The constructed CNN model.
-    """
     dropoutrate = 0.85
     input_layer = Input(shape=(windowSize, numberChannels, 1))
     unit1 = Conv2D(filters=21,
@@ -153,35 +116,19 @@ def construct_model(windowSize, numberChannels):
     output_layer = Dense(2, activation='softmax')(l5out)
     model = Model(inputs=input_layer, outputs=output_layer)
     return model
-
-
-def load_matfile(filename, windowSize, datax, datay):
-    """
-    Loads and preprocesses the MATLAB file containing EEG data.
-
-    Args:
-        filename (str): The path to the MATLAB file.
-        windowSize (int): The size of the time window for EEG data.
-        datax (str): The key for training data in the MATLAB file.
-        datay (str): The key for training labels in the MATLAB file.
-
-    Returns:
-        data_x_train (np.array): Preprocessed training data.
-        data_y_train (np.array): Preprocessed training labels.
-        data_x_val (np.array): Preprocessed validation data.
-        data_y_val (np.array): Preprocessed validation labels.
-        data_x_test (np.array): Preprocessed test data.
-    """
+# LOAD MATLAB FILE
+def load_matfile(filename, windowSize):
     mat_contents = sio.loadmat(filename)
-    train_data_x = np.array(mat_contents[datax])
-    train_data_y = np.array(mat_contents[datay])
+    train_data_x = np.array(mat_contents['train_data_x'])
+    train_data_y = np.array(mat_contents['train_data_y'])
     test_data_x = np.array(mat_contents['test_data_x'])
+
 
     channels = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21]) - 1
     train_data_x = train_data_x[:, channels, :]
     test_data_x = test_data_x[:, channels, :]
 
-    # Split train data into 250ms (150 sample) windows
+    # split train data to 250ms (150 sample) windows
     data_x_train = []
     data_y_train = []
     for ii in range(train_data_x.shape[0]):
@@ -190,7 +137,6 @@ def load_matfile(filename, windowSize, datax, datay):
         for t in range(windowSize):
             data_x_windows[:, t, :] = np.roll(trialdata, -t, axis=0)
         data_x_windows = data_x_windows[0:(data_x_windows.shape[0] - windowSize), :, :]
-
         bitdata = train_data_y[ii, :].squeeze().transpose()
         bitdata = np_utils.to_categorical(bitdata, 2)
         for t in range(data_x_windows.shape[0]):
@@ -199,7 +145,7 @@ def load_matfile(filename, windowSize, datax, datay):
     data_x_train = np.array(data_x_train);
     data_y_train = np.array(data_y_train);
 
-    # Split train data into train and validation sets (equal size)
+    # split train data to train and validation set (equal size)
     data_x_train = data_x_train.reshape(-1, data_x_train.shape[1], data_x_train.shape[2], 1)
     data_y_train = data_y_train.reshape(-1, 2)
     if (len(data_x_train) % 2) != 0:
@@ -212,7 +158,7 @@ def load_matfile(filename, windowSize, datax, datay):
     data_x_val = x_split[1]
     data_y_val = y_split[1]
 
-    # Split test data into 250ms (150 sample) windows
+    # split test data to 250ms (150 sample) windows
     data_x_test = np.zeros((test_data_x.shape[0], test_data_x.shape[2] - windowSize, windowSize, test_data_x.shape[1]))
     for ii in range(test_data_x.shape[0]):
         trialdata = test_data_x[ii, :, :].squeeze().transpose()
@@ -222,43 +168,36 @@ def load_matfile(filename, windowSize, datax, datay):
         data_x_windows = data_x_windows[0:(data_x_windows.shape[0] - windowSize), :, :]
         data_x_test[ii, :, :, :] = data_x_windows.reshape(-1, data_x_windows.shape[0], data_x_windows.shape[1],
                                                           data_x_windows.shape[2])
-
+    data_x_test = np.array(data_x_test)
+    plt.figure(figsize=(12, 8), dpi=80)
+    plt.plot(data_x_test[0, 0, :, 0])
     return data_x_train, data_y_train, data_x_val, data_y_val, data_x_test
 
 
 def downsample(arr, n):
-    """
-    Downsamples an array by averaging over every n elements.
-
-    Args:
-        arr (np.array): The input array.
-        n (int): The downsampling factor.
-
-    Returns:
-        np.array: The downsampled array.
-    """
     end = n * int(len(arr) / n)
     return np.mean(arr[:end].reshape(-1, n), 1)
-
-
-# Main execution
 num_args = len(sys.argv) - 1
 for i in range(num_args):
     MATLAB_FILE = sys.argv[i+1]
     MODEL_FILE = MATLAB_FILE[:-4] + '.hdf5'
 
     ## PARAMETERS
-    WINDOW_SIZE = 160  # Equals 250ms at 600Hz sampling rate
-    lr = 0.0005  # Learning rate
-    batchsize = 256  # Batch size
-    epochs = 10  # Number of epochs
+    WINDOW_SIZE = 160 # equals 250ms at 600Hz sampling  grate
+    lr = 0.0005  # the learning rate
+    batchsize = 256  # the number of epochs
+    epochs = 10  # t0.81,0.99;0.87,0.93;0.85,0.95;0.87,0.93;0.85,0.95;0.87,0.9;0.83,0.9;0.85,0.93;0.76,0.83;0.79,0.88;0.75,0.82he number of epochs hby6
 
-    ## LOAD DATA
-    DATA_X = 'train_datax3012'
-    DATA_Y = 'train_data_y3012'
-    (data_x_train, data_y_train, data_x_val, data_y_val, data_x_test) = load_matfile(MATLAB_FILE, WINDOW_SIZE, DATA_X, DATA_Y)
 
-    ## CREATE AND TRAIN EEG2Code CNN MODEL
+    ## CREATE EEG2Code CNN Model
+
+
+
+    ## LOAD data
+    (data_x_train, data_y_train, data_x_val, data_y_val, data_x_test) = load_matfile(MATLAB_FILE, WINDOW_SIZE)
+
+    ## TRAIN EEG2Code CNN Model
+
     model = construct_model(data_x_train.shape[1], data_x_train.shape[2])
     adam = tf.keras.optimizers.Adam(lr=lr)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
@@ -266,22 +205,41 @@ for i in range(num_args):
                         validation_data=(data_x_val, data_y_val), callbacks=[
             keras.callbacks.ModelCheckpoint(MODEL_FILE, monitor='val_loss', verbose=0, save_best_only=True,
                                             save_weights_only=False, mode='auto', period=1)])
-    tf.keras.backend.clear_session()
 
-    ## PREDICT ON TEST DATA
-    preddata1 = np.zeros((data_x_test.shape[0], data_x_test.shape[1], 2))
+    # 查看模型层及参数
+    # deepxi.model.summary()
+    #
+    # # 将模型结构保存为图片
+    # model_img_name = args.ver + '-' + args.network_type + '.png'
+    # keras.utils.plot_model(deepxi.model, model_img_name, show_shapes=True)
+
+    ## EEG2Code prediction
+    preddatall = np.zeros((data_x_test.shape[0], data_x_test.shape[1], 2))
+    ## EEG2Code prediction
     for ii in range(data_x_test.shape[0]):
         data_x_test_run = data_x_test[ii, :, :, :].squeeze()
-        x = data_x_test_run.reshape(-1, data_x_test_run.shape[1], data_x_test_run.shape[2], 1)
-        # Perform EEG2Code prediction (sample-wise)
-        preddata = model.predict(data_x_test_run.reshape(-1, data_x_test_run.shape[1], data_x_test_run.shape[2], 1))
-        preddata1[ii, :] = preddata.squeeze()
 
-    ## SAVE RESULTS
-    RESULT_FILE = MATLAB_FILE[:-4] + 'result.mat'  # Here, name the output file.
+        x = data_x_test_run.reshape(-1, data_x_test_run.shape[1], data_x_test_run.shape[2], 1)
+        # do EEG2Code prediction (sample-wise)
+        preddata = model.predict(data_x_test_run.reshape(-1, data_x_test_run.shape[1], data_x_test_run.shape[2], 1))
+        preddatall[ii, :] = preddata.squeeze()
+    RESULT_FILE = MATLAB_FILE[:-4] + 'result.mat'
+    scipy.io.savemat(RESULT_FILE, {'data': preddatall})
+
     tf.keras.backend.clear_session()
+
+
+
     tf.compat.v1.reset_default_graph()
+
     tf.compat.v1.keras.backend.clear_session()
+
+  
     del data_x_train, data_y_train, data_x_val, data_y_val, data_x_test, model
-    gc.collect()
-    scipy.io.savemat(RESULT_FILE, {'preddata1': preddata1})
+
+    gc.collect()  
+
+
+
+    time.sleep(60)
+
